a9cc4ef864b83dfa226145b4421ac3c85e5d732b
ilan7empest <63646693+ilan7empest@users.noreply.github.com>
2021-09-03 16:06:58 +0300

    moved file formValid

--- src/context/formContext.js
+++ src/context/formContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from 'react';
 
-import { useFormValid } from '../hooks/useFormValid';
+import { useFormValid } from '../utils/useFormValid';
 
 const FormContext = createContext();
 
